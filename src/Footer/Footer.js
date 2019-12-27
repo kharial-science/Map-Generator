59a7083227a0db8c6c1baa7cb752e88f9faafb11
Kharoh <Gaulu03@gmail.com>
2019-12-27 19:48:34 +0100

    designing footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -10,9 +10,10 @@ class Footer extends Component {
   render () {
     return (
       <div id='Footer'>
-        <a href="https://twitter.com/KFamilyScience" className="social-network"><img href="../assets/twitter.png" alt="twitter" /></a>
-        <a href="https://github.com/kharohfamily-science" className="social-network"><img href="" alt="github" /></a>
-        <a href="https://kharohfamily-science.github.io/Kharoh-Family-Science/" className="social-network"><img href="" alt="website" /></a>
+        {/* <a href="https://kharohfamily-science.github.io/Kharoh-Family-Science/" className="social-network"><img src={require("../assets/website.png")} alt="website" /></a> */}
+        <a href="https://discord.gg/AUTXwEA" className="social-network"><img src={"https://img.icons8.com/dotty/80/000000/discord-logo.png"} alt="discord" /></a>
+        <a href="https://github.com/kharohfamily-science" className="social-network"><img src={"https://img.icons8.com/dotty/80/000000/github.png"} alt="github" /></a>
+        <a href="https://twitter.com/KFamilyScience" className="social-network"><img src={"https://img.icons8.com/dotty/80/000000/twitter.png"} alt="twitter" /></a>
         {/* <iframe src="https://discordapp.com/widget?id=651121972763164673&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe> */}
       </div>
     )
