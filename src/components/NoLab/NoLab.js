afeb1c20c2d044bbb5e9e8206e4348ee0dff0a11
SuperMighty0 <46725116+SuperMighty0@users.noreply.github.com>
2019-12-29 11:55:28 +0100

    Now explain to pad user that they can access to lab in landscape mode

--- src/components/NoLab/NoLab.js
+++ src/components/NoLab/NoLab.js
@@ -7,7 +7,7 @@ const NoLab = (props) => {
     <div id='NoLab'>
       <div className='no-lab-container'>
         <img src='https://img.icons8.com/dusk/64/000000/lock-2.png' alt='lock icon' />
-        <h2>Check the PC version in order to find The Lab !</h2>
+        <h2>{window.screen.orientation == "landscape-primary" || window.screen.orientation == "landscape-secondary" ? "Check the PC version in order to find The Lab !" : "Switch to landscape mode in order to find the Lab"}</h2>
       </div>
     </div>
   )
